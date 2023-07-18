c7471d66519644af3c6648a9e53e7441dd881aff
hehongd <635418133@qq.com>
2023-07-18 17:05:24 +0800

    完成了搜索功能的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,7 @@
 <template>
   <view>
+    <!-- 使用自定义的搜索组件 -->
+    <my-search @click="gotoSearch"></my-search>
     <view class="scroll-view-container">
       <!-- 左侧的滚动视图区域 -->
       <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
@@ -48,8 +50,8 @@
     onLoad() {
       // 获取当前系统的信息
       const sysInfo = uni.getSystemInfoSync()
-      // 为 wh 窗口可用高度动态赋值
-      this.wh = sysInfo.windowHeight
+      // 可用高度 = 屏幕高度 - navigationBar高度 - tabBar高度 - 自定义的search组件高度
+      this.wh = sysInfo.windowHeight - 50
       // 调用获取分类列表数据的方法
       this.getCateList()
     },
@@ -57,6 +59,7 @@
       async getCateList() {
         // 发起请求
         const { data: res } = await uni.$http.get('/api/public/v1/categories')
+        console.log(res)
         // 判断是否获取失败
         if (res.meta.status !== 200) return uni.$showMsg()
         // 转存数据
@@ -81,7 +84,13 @@
         uni.navigateTo({
           url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
         })
-      }
+      },
+     // 跳转到分包中的搜索页面
+     gotoSearch() {
+       uni.navigateTo({
+         url: '/subpkg/search/search'
+       })
+     }
     }
   }
 </script>
